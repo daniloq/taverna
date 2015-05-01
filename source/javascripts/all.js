3d58f7cbfe0395293d4c6297b505b3762cbc96a1
Danilo Quilaton <daniloquilaton@gmail.com>
2015-04-30 23:17:22 -0400

    Removes unused css and javascript

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,19 +1,2 @@
-//= require 'moment'
-//= require 'OpeningHours'
 //= require 'instafeed'
 //= require_self
-
-$(function() {
-
-    $.getJSON('/tests/multipleWeeks_version.json', function(data) {
-        if (data) {
-            $('.openingHours').openingHours({
-                json: data,
-                lang: 'ro',
-                daysForm: 'normal',
-                weekPeriods: true
-            });
-        }
-    });
-
-});
