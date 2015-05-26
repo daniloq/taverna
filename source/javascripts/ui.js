00b24052a0205ac9a759018feda1243fbd960201
Amber Aultman <amber.aultman@gmail.com>
2015-05-26 17:05:16 -0400

    adding loader image to carousel

--- source/javascripts/ui.js
+++ source/javascripts/ui.js
@@ -87,7 +87,7 @@ $('.next').click(function() {
 // Background Image Cycle
 $(window).load(function() {
   var i = 0;
-  var images = ['images/1.jpg','images/2.jpg','images/3.jpg','images/4.jpg' ];
+  var images = ['images/1.jpg','images/2.jpg','images/3.jpg','images/4.jpg', 'images/hero-1.jpg' ];
   var image = $('.hero');
   //Initial Background image setup
   image.css('background', 'url(images/hero-1.jpg)');
