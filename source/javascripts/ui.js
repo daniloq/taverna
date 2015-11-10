afc1bb080d54c2c0705493f0ff4d3d8e1d1a599d
Amber Aultman <amber.aultman@gmail.com>
2015-11-09 20:57:46 -0500

    menu and image updates

--- source/javascripts/ui.js
+++ source/javascripts/ui.js
@@ -59,7 +59,7 @@ $(document).ready(function() {
 
   // New Background Image Slider
   $('.hero').bkgSlide({
-      images: ['images/1.jpg','images/2.jpg','images/3.jpg','images/4.jpg','images/5.jpg'],
+      images: ['images/6.jpg','images/7.jpg','images/8.jpg','images/9.jpg','images/10.jpg'],
       speed: 8000,
       width: '100%',
       height: 'auto'
